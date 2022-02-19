550e6bfb0b2c81eff56ecb4d818181c25133c7a3
eaniko <69327188+eaniko@users.noreply.github.com>
2022-02-19 03:27:01 +0100

    nemvégleges

--- js-1-feladatok-tictactoe/tic-tac-toe - template.js
+++ js-1-feladatok-tictactoe/tic-tac-toe - template.js
@@ -7,12 +7,6 @@ Hozd létre a szükséges változókat, és definiáld is őket:
 - mark: ez a jel, amellyel játszanak a játékosok (0 vagy X, alapértelmezetten 'X').
 */
 
-let matrix = new Array(2);
-let stepCount = 0;
-let cols = 3;
-let rows = 3;
-mark = "X";
-
 /**
 Fejtsd ki az initState elnevezésű függvényt, 
 amely feltölti üres mezőkkel a játékteret, 
@@ -33,20 +27,8 @@ a sorok száma tömbbé alakítva,
 és feltöltve null-lal.
 @returns void (nem ad vissza semmit)
 */
-const initState = (array) => {
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			array[i][j] = 0;
-		}
-	}
-};
-
-initState(matrix);
-for (var i = 0; i < cols; i++) {
-	for (var j = 0; j < rows; j++) {
-		console.log(matrix[i][j] + " ");
-	}
-	console.log("\n");
+const initState = () => {
+    
 }
 
 /**
@@ -70,30 +52,36 @@ A datasetről itt olvashatsz bővebben:
 https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
 */
 const changeMatrixValue = (element) => {
-	const row = parseInt(element.dataset.row, 10);
-	const cell = parseInt(element.dataset.cell, 10);
-	matrix[row][cell] = element.textContent;
-};
+    const row = parseInt(element.dataset.row, 10);
+    const cell = parseInt(element.dataset.cell, 10);
+    matrix[row][cell] = element.textContent;
+}
 
 /**
 Fejtsd ki a deleteSigns elnevezésű függvényt, 
 amely kiválasztja az összes cellát, 
 és mindegyik elemben elhelyez egy üres string-et.
 */
-const deleteSigns = () => {};
+const deleteSigns = () => {
+   
+}
 
 /**
 Fejtsd ki az increaseCounter elnevezésű függvényt, 
 amely a megtett lépések számát növeli eggyel.
 */
-const increaseCounter = () => {};
+const increaseCounter = () => {
+    
+}
 
 /**
 Fejtsd ki a modifyCell elnevezésű függvényt, 
 amely beállítja az elem tartalmának a használt jelet,
 majd kattintásra (esemény) eltávolítja a handleClick függvényt. 
 */
-const modifyCell = (element) => {};
+const modifyCell = (element) => {
+    
+}
 
 /**
 Fejtsd ki a setMark elnevezésű függvényt, 
@@ -102,7 +90,9 @@ hogy ha az X-et használtuk éppen, azaz a jel === X,
 akkor beállítja a 0-t, hiszen a másik játékos azzal fog tenni egy jelet,
 ha pedig a 0-t használtuk, beállítja az X-et.
 */
-const setMark = () => {};
+const setMark = () => {
+    
+}
 
 /**
 Fejtsd ki a handleClick elnevezésű függvényt, 
@@ -113,21 +103,27 @@ amely meghívja a következő függvényeket:
 - changeMatrixValue(event.target)
 - checkWinner()
 */
-const handleClick = (event) => {};
+const handleClick = (event) => {
+   
+}
 
 /**
 Fejtsd ki az addClickListener elnevezésű függvényt, 
 amely kiválasztja a cellákat, 
 és kattintásra (esemény) mindegyikhez hozzáadja a handleClick függvényt.
 */
-const addClickListener = () => {};
+const addClickListener = () => {
+  
+}
 
 /**
 Fejtsd ki a removeAllClickListeners elnevezésű függvényt, 
 amely kiválasztja a cellákat, 
 és kattintásra (esemény) mindegyikről eltávolítja a handleClick függvényt.
 */
-const removeAllClickListeners = () => {};
+const removeAllClickListeners = () => {
+ 
+}
 
 /**
 Fejtsd ki a checkValues elnevezésű függvényt, 
@@ -138,45 +134,44 @@ Ha a sor minden egyes eleme 0 vagy X,
 akkor a 0 vagy az X győzött.
 Ha valaki győzött, akkor pl. egy ilyen tömböt kapunk: [true, false, false]
 */
-const checkValues = (array) =>
-	array
-		.map((row) => {
-			/*Ide írd a kódot!*/
-		})
-		.indexOf(true) !== -1;
-/*
+const checkValues = (array) => array.map(row => {/*Ide írd a kódot!*/})
+    .indexOf(true) !== -1;
+    /*
     Ha true-t kapunk visza adott sorra, akkor 
     annak indexét vizsgálva nem kaphatunk -1-et.
     Azaz az elem benne van a tömbben.
     */
 
-const checkColumnValues = () =>
-	checkValues(matrix.map((array, i) => array.map((item, j) => matrix[j][i])));
+    const checkColumnValues = () => 
+        checkValues(matrix.map((array, i) => 
+        array.map((item, j) => matrix[j][i])));
 
-const checkDiagonalValues = () =>
-	checkValues([
-		matrix.map((array, i) => matrix[i][i]),
-		matrix.map((array, i) => matrix[i][matrix[i].length - i - 1]),
+    const checkDiagonalValues = () =>
+        checkValues([
+        matrix.map((array, i) => matrix[i][i]),
+        matrix.map((array, i) => matrix[i][matrix[i].length - i - 1])
 
-		/*
+    /*
     Miután az első függvénnyel leellenőriztük a sorok tartalmát,
     a fentiekkel megvizsgáljuk az oszlopok és az átlókban lévő mezők tartalmát.
     Ez azért fontos, mert így tudhatjuk meg, 
     hogy lesz-e három azonos jel egymás mellett/alatt/átlósan.
     */
-	]);
+]);
 
 /*
 Fejtsd ki a checkWinner elnevezésű függvényt, 
 amely...
 */
 const checkWinner = () => {
-	// ...kiírja a konzolra a checkColumnValues()t és a checkDiagonalValues()-t,
-	/*
+    // ...kiírja a konzolra a checkColumnValues()t és a checkDiagonalValues()-t,
+    
+    /*
     majd meghívja az endGame()-et, ha hogy HA
     a checkValues(matrix) vagy a checkColumnValues() vagy a checkDiagonalValues() igaz.
     */
-};
+
+}
 
 /**
 A HTML-ben a játékteret követően vegyél fel egy divet message osztállyal,
@@ -186,7 +181,9 @@ Fejtsd ki a setMessage elnevezésű függvényt,
 amely kiválasztja a message osztályú elemet, 
 és az üzenetet állítja be a div tartalmának.
 */
-const setMessage = (message) => {};
+const setMessage = (message) => {
+    
+}
 
 /**
 Fejtsd ki a startGame elnevezésű függvényt, 
@@ -195,7 +192,9 @@ amely meghívja a következő függvényeket:
 - addClickListener()
 - newGame()
 */
-const startGame = () => {};
+const startGame = () => {
+    
+}
 
 /**
 Fejtsd ki az endGame elnevezésű függvényt, 
@@ -210,7 +209,9 @@ Ez az utóbbi kódrészlet kiválasztja azt a jelet, amellyel a nyertes játszot
 
 Ezután a függvény meghívja a removeAllClickListeners() nevű függvényt.
 */
-const endGame = () => {};
+const endGame = () => {
+    
+}
 
 /*
 Indíts el egy új játékot az alábbi függvény segítségével!
@@ -218,8 +219,9 @@ Indíts el egy új játékot az alábbi függvény segítségével!
 Hozz létre a HTML-ben a játéktéren kívül egy gombot!
 */
 const newGame = () => {
-	// Válaszd ki a gombot!
-	/*
+    // Válaszd ki a gombot!
+    
+    /*
     Tegyél rá/adj hozzá egy eseményfigyelőt, 
     amely kattintásra meghívja a következő függvényeket:
         - initState()
@@ -228,6 +230,7 @@ const newGame = () => {
         - setMessage('Playing...')
         - setMark()
         */
-};
+          
+}
 
 startGame();
